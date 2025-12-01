e13f99fa05f9a80021997eabfc56e4df337bad39
withsalt <sysrun@163.com>
2025-12-01 22:47:07 +0800

    fix: update

--- src/noVNCClient/Program.cs
+++ src/noVNCClient/Program.cs
@@ -8,23 +8,21 @@ namespace noVNCClient
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            // Read Websockify configuration from appsettings.json
+            var websockifyOptions = new WebsockifyOptions();
+            builder.Configuration.GetSection("Websockify").Bind(websockifyOptions);
 
             builder.Services.AddMemoryCache();
-
             // Add services to the container.
             builder.Services.AddControllers();
 
             var app = builder.Build();
 
             app.UseRouting();
-
-            // Read Websockify configuration from appsettings.json
-            var websockifyOptions = new WebsockifyOptions();
-            builder.Configuration.GetSection("Websockify").Bind(websockifyOptions);
-
             // Add the Websockify middleware to the application pipeline.
             app.UseWebsockify(websockifyOptions.Path, websockifyOptions.Host, websockifyOptions.Port);
-
+            
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
